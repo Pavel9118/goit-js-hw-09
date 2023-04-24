59053d1521c9ae8bea812fb99d3a7aaead64afd9
Pavel9118 <116108521+Pavel9118@users.noreply.github.com>
2023-04-24 03:26:36 +0000

    Deploying to gh-pages from @ Pavel9118/goit-js-hw-09@af6a1e6192611651fbaf01a76d504c6d50022695 🚀

---
+++ 01-color-switcher.7e4adeac.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),bodyColor:document.querySelector("body")};function e(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}t.start.addEventListener("click",(function(){t.start.disabled=!0,t.stop.disabled=!1,timerId=setInterval(e,1e3)})),t.stop.addEventListener("click",(function(){t.start.disabled=!1,t.stop.disabled=!0,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.7e4adeac.js.map
